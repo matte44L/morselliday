69f34fb15ce5a77e6a0ed145e28380e970f8d93b
matte44L <crocematteo116@gmail.com>
2025-05-24 08:38:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const track = document.querySelector('.carousel-track');
 const clones = track.innerHTML;
 track.innerHTML += clones; 
+function toggleCaption(button) {
+  const caption = button.nextElementSibling;
+  button.classList.toggle("open");
+  caption.classList.toggle("visible");
+}
